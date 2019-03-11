54f6ce9c09e6bef7d567cdcfbdf698a39bee42e2
Manny Barboza <mannyb1077@gmail.com>
2019-03-10 22:37:47 -0600

    Need styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,347 +1,213 @@
+var start;
 
-var timer = false;
-var intervalID;
-var time = 5;
-var enabled = 0;
+var triviaHTML;
 
-var questionObject =
+var counter = 30;
+
+var questions = 
+    [ "Which of these U.S. Presidents appeared on the television series Laugh-In?",
+
+    "Colonized in the 16th century, what country took its name from the first name of the king of Spain?", 
+
+    "Long after the same business made the family rich, in 2014, heirs of John D Rockefeller announced plans to stop investing their charity funds in what?", 
+
+    "Despite many movie portrayals, Wikihow says doing what, is rare, not normal and suggest you seek psychological help if you're considering it?", 
+
+    "Considered the world's largest music instrument, the Greatest Stalacpipe Organ plays music exclusively where?",
+
+    "Despite it being in what is now Instabul, over 1,000 miles from Scandinavia, what famous landmark is believed to have once been vandalized by Vikings?", 
+
+    "Which of these is the name of both the Oscar-nominated director of 12 years a Slave and an Oscar-nominated actor dubbed The King of Cool ?", 
+
+    "With its lyric, And no one heard at all/Not even the chair, What Neil Diamond hit reportedly inspired Clint Eastwood's infamous chair stunt at the 2012 RNC?",
+
+    "Hearing that Guiness World Records was going to nme him the World's most litigous person, Jonathan Lee Riches responded by what?",
+
+    "When Steve Jobs accused Bill Gates of ripping off Apple, Gates said they'd both stolen from what tech company that developed the mouse?" ];
+
+var answers = [
+    ["Lyndon Johnson", "Richard Nixon", "Jimmy Carter", "Gerald Ford"], 
+    ["Eduardor","Kevinzuela","The Phillipines","Stuganda"], 
+    ["Railroads", "Fossil Fuels", "Diamond Mining", "Steel"], 
+    ["Planning a wedding", "Dancing at a wedding", "Stop a wedding", "Officiating a wedding"], 
+    ["Under Water", "Under the night sky", "In a jungle", "In a cave"], 
+    ["Hagia Sophia","Dome of the Rock","Persepolis","Taj Mahal"], 
+    ["Paul Newman", "Steve McQueen", "Rober Mitchum", "Richard Burton"], 
+    ["Love on the Rocks","Sweet Caroline","Song Sue Blue","I am...I Said"], 
+    ["Thanking them","Suing them","Interviewing them","Funding them"],
+    ["Hewlett-Packard","Xerox","Intel","IBM"]];
+ 
+var correctAnswers = [ 
+    "B. Richard Nixon",
+    "C. The Phillipines", 
+    "B. Fossil Fuels", 
+    "C. Stop a wedding", 
+    "D. In a cave", 
+    "A. Hagia Sophia", 
+    "B. Steve McQueen", 
+    "D. I am...I Said",
+    "B. Suing them",
+    "B. Xerox"];
+
+var questionCounter = 0;
+var selectedAnswer;
+var timer;
+var wins = 0;
+var losses = 0;
+var noResponse = 0;
+
+$(document).ready(function() 
 {
-     question1:
-      {
-        q1: "Question1",
-        a1: "Answer1",
-        enabled: false,
-        asked: 0,
-        
-      },
-      question2:
-      {
-        q2: "Question2",
-        a2: "Answer2",
-        enabled: false,
-        asked: 0,
-      },
-      question3:
-      {
-        q3: "Question3",
-        a3: "Answer3",
-        enabled: false,
-        asked: 0,
-        
-      },
-      question4:
-      {
-        q4: "Question4",
-        a4: "Answer4",
-        enabled: false,
-        asked: 0,
-        
-      },
-      question5:
-      {
-        q5: "Question5",
-        a5: "Answer5",
-        enabled: false,
-        asked: 0,
-        
-      },
-    //   question2: 
-    //   [{
-    //     type: "home",
-    //     number: "212 555-1234"
-    //    }, 
-    //    {
-    //     type: "fax",
-    //     number: "646 555-4567"
-    //    }]
-    };
-// var questions = ["Question 1", "Question 2","Question 3","Question 4","Question 5"];
-// var answers = ["Answer 1","Answer 2","Correct Answer q1","Answer 4","Answer 5"];
-var randomQuestion = [];
-var answer = [];
-var answeredCorrect = [];
-var answeredIncorrect = [];
-var allQuestions = [];
-
-
-//TEST
-
-
-
-
-function triviaQuestions(question, answer, enabled, asked) 
-{
-    this.question = question;
-    this.answer = answer;
-    this.enabled = enabled;
-    this.asked = asked;
-}
-
-
-$("#button-clear").click(function () 
-{
-    var questionButton = $('<button>Test</button>').click(function () 
+  
+    function startGame() 
     {
-        alert('hi');
-    });
+        startButton = "<p class='text-center main-button-container'><a class='btn btn-warning btn-md btn-block start-button' href='#' role='button'>Start Game</a></p>";
 
-    $("#button-answer1").css('display', 'table-row'); // NB: changed
+        $("#mainArea").append(startButton);
+    }
+    
+    startGame();
+    
 
-    var tr = $('<tr>').insertBefore('#addNodeTable tr:last');
-    var td = $('<td>').append(questionButton).appendTo(tr);
+    $("#mainArea").on("click", ".start-button", function(event)
+    {
+    //    event.preventDefault();  // added line to test issue on GitHub Viewer
+        
+        $('.jumbotron').hide(); 
+        loadQuestions();
+    
+        timerWrapper();
+    
+    }); 
+    
+    $("body").on("click", ".answer", function(event)
+    {
+  
+        selectedAnswer = $(this).text();
+        
+        if(selectedAnswer === correctAnswers[questionCounter])
+        {
+            clearInterval(timer);
+            loadWin();
+        }
+        else
+        {
+            clearInterval(timer);
+            loadLoss();
+        }
+
+        console.log(selectedAnswer);
+        console.log(correctAnswers[questionCounter]);
+    }); 
+    
+    $("body").on("click", ".reset-button", function(event)
+    {
+        // clickSound.play();
+        resetGame();
+    }); 
+    
+    
+});  
+
+    function timeout() 
+    {
+        noResponse++;
+        triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='/Users/MannyBarboza/Developer/uubc/sandbox/Homework/Homework5/TriviaGame/assets/images/timeout.gif'>";
+        $("#mainArea").html(triviaHTML);
+        setTimeout(wait, 5000); 
+    }
+    
+    function loadWin() 
+    {
+        wins++;
+        
+        triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='/Users/MannyBarboza/Developer/uubc/sandbox/Homework/Homework5/TriviaGame/assets/images/correctanswer.gif'>";
+
+        $("#mainArea").html(triviaHTML);
+        
+        setTimeout(wait, 5000);  
+    }
+    
+    function loadLoss() 
+    {
+        losses++;
+        triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='/Users/MannyBarboza/Developer/uubc/sandbox/Homework/Homework5/TriviaGame/assets/images/wronganswer.gif'>";
+        $("#mainArea").html(triviaHTML);
+        
+        setTimeout(wait, 5000); 
+    }
+    
+
+    function loadQuestions() 
+    {
+        triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questions[questionCounter] + "</p><p class='first-answer answer'>A. " + answers[questionCounter][0] + "</p><p class='answer'>B. "+answers[questionCounter][1]+"</p><p class='answer'>C. "+answers[questionCounter][2]+"</p><p class='answer'>D. "+answers[questionCounter][3]+"</p>";
+
+        $("#mainArea").html(triviaHTML);
+    }; 
+    
+    function wait() 
+    {
+        if (questionCounter < 10)
+        {
+            questionCounter++
+            loadQuestions();
+            counter = 30,
+            timerWrapper();
+     
+        }
+        else
+        {
+            finalScreen();
+        }
+        console.log(questionCounter);
+
+    //     questionCounter < 10 ?
+
+    //     (questionCounter++,
+    //     generateQuestions(),
+    //     counter = 30,
+    //     timerWrapper() ):
+        
+    //    (finalScreen())
+    //    console.log(questionCounter);
+    }; 
+    
+    function timerWrapper() 
+    {
+        timer = setInterval(thirtySeconds, 1000);
+
+        function thirtySeconds() 
+        {
+            if (counter === 0) 
+            {
+                clearInterval(timer);
+                timeout();
+            }
+            if (counter > 0) 
+            {
+                counter--;
+            }
+            $(".timer").html(counter);
+        }
+    }
+    
+    function finalScreen() 
+    {
+        triviaHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + wins + "</p>" + "<p>Wrong Answers: " + losses + "</p>" + "<p>Unanswered: " + noResponse + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-warning btn-md btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+        $("#mainArea").html(triviaHTML);
+    }
+    
+    function resetGame() 
+    {
+        questionCounter = 0;
+        wins = 0;
+        losses = 0;
+        noResponse = 0;
+        counter = 30;
+        loadQuestions();
+        timerWrapper();
+    }
+    
    
-});
-
-$("#button-2").click(function () 
-{
-    console.log(questionObject.question1.q1);
-    console.log(questionObject.question1.a1);
-    console.log(questionObject.question1.enabled);
-    console.log(questionObject.question1.asked);
-    questionObject.question1.asked++
-    questionObject.question1.enabled = true;
-
-});
-
-$("#button-3").click(function () 
-{
-    console.log(questionObject.question2.q2);
-    console.log(questionObject.question2.a2);
-    console.log(questionObject.question2.enabled);
-    console.log(questionObject.question2.asked);
-    questionObject.question2.asked++
-    questionObject.question2.enabled = true;
-});
-
-
-// document.getElementById("#button-answer3").style.visibility=hidden;
-// document.getElementById(".buttonAnswers").innerHTML="You got " + correct.toExponential; 
-
-//End of Test
-
-
-
-window.onload = function() 
-{
-    startTimer();
-    loadNewQuestion();
-    loadAnswers();
-};
-
-
-
-
-function startTimer()
-{
-    if (!timer) 
-    {
-        intervalId = setInterval(count, 1000);
-        timer = true; 
-    }
-    
-}
-
-function resetTimer()
-{
-    time = 5;
-    clearInterval(intervalID); 
-    timer = true;
-  
-    $("#button-timer").text("00:" + time);
-  
-}
-
-function stopTimer()
-{
-    clearInterval(intervalId);
-    timer = false;
-}
-
-function timerExpired()
-{
-    time = 5;
-    loadNewQuestion();
-    // console.log("This is the length of the Questions" + questions.length)
-    // console.log("This is All Questions " + allQuestions)
-
-    if (allQuestions.length == questionObject.length)
-    {
-        stopTimer();
-        timer = false;
-        alert("Game Over");
-        allQuestions = [];
-    }
-    else
-    {
-        alert("Timer Expired, Waiting for New Question")
-    }
-    
-
-}
-
-
-function loadNewQuestion()
-{
-    var randomNum
-    for (i = 0; i > questionObject.length; i++)
-    {
-      randomNum = i;  
-      
-    }
-    console.log(randomNum);
-
-
-    randomQuestion = questionObject[randomNum]
-    $("#button-question").text(randomQuestion);
-
-    allQuestions.push(randomQuestion);
-   
-    // var randomNum
-    // randomNum =Math.floor(Math.random()* questions.length);
-    // randomQuestion = questions[randomNum]
-    // $("#button-question").text(randomQuestion);
-
-    // allQuestions.push(randomQuestion);
-    
    
     
-}
-
-function loadAnswers()
-{
-    
-    // var answer1 =Math.floor(Math.random()* answerObject.length);
-    // var answer2 =Math.floor(Math.random()* answerObject.length);
-    // var answer3 =Math.floor(Math.random()* answerObject.length);
-    // var answer4 =Math.floor(Math.random()* answerObject.length);
-    // var answer5 =Math.floor(Math.random()* answerObject.length);
-    
-    // randomAnswer1 =answerObject[answer1];
-    // randomAnswer2 =answerObject[answer2];
-    // randomAnswer3 =answerObject[answer3];
-    // randomAnswer4 =answerObject[answer4];
-    // randomAnswer5 =answerObject[answer5];
-    
-
-    // $("#button-answer1").text(randomAnswer1);
-    // $("#button-answer2").text(randomAnswer2);
-    // $("#button-answer3").text(randomAnswer3);
-    // $("#button-answer4").text(randomAnswer4);
-    // $("#button-answer5").text(randomAnswer5);
-}
-
-function questionAnsweredCorrectly()
-{
-    var question1 = document.trivia.question1.value;
-    var question2 = document.trivia.question2.value;
-    var question3 = document.trivia.question3.value;
-    var question4 = document.trivia.question4.value;
-    var question5 = document.trivia.question5.value;
-
-    var correct = 0;
-
-    if ( question1 == "q1AnswerCorrect")
-    {
-        correct ++;
-    }
-    if ( question2 == "q2AnswerCorrect")
-    {
-        correct ++;
-    }
-    if ( question3 == "q3AnswerCorrect")
-    {
-        correct ++;
-    }
-    if ( question4 == "q4AnswerCorrect")
-    {
-        correct ++;
-    }
-    if ( question5 == "q5AnswerCorrect")
-    {
-        correct ++;
-    }
-
-    document.getElementById("btnanswer").style.visibility=visible;
-    document.getElementById("btnCorrect").innerHTML="You got " + correct.toExponential; 
-
-}
-
-function questionAnsweredIncorrectly ()
-{
-    startTimer();
-
-}
-
-function submit()
-{
-
-}
-
-function count() 
-{
-    time--;
-  
-    var converted = timeConverter(time);
-    // console.log("Converted " + converted);
-  
-    $("#button-timer").text(converted);
-
-    if (time < 1)
-    {
-        timerExpired();
-
-    }
-}
-    
-
-function timeConverter(t) 
-{
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 60);
-
-    if (seconds < 10) 
-    {
-      seconds = "0" + seconds;
-    }
-  
-    if (minutes === 0) 
-    {
-      minutes = "00";
-    }
-    else if (minutes < 10) 
-    {
-      minutes = "0" + minutes;
-    }
-    
-    return minutes + ":" + seconds;
-    
-
-}
-
-
-
-// if (on.click == answer[4])//correct answer
-// if (on.click !== correctanswer)//You lose
-
-function getTime()
-{
-    const date = new Date();
-    console.log(date);
-
-    return date.getHours() + ";" + date.getMinutes();
-}
-// const time = getTime();
-// console.log(time);
-
-
-                //Create new Buttons///////
-// var tRow = $("<tr>");
-// // Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
-//   var titleData = $("<td>").text(response.Title);
-//   var yearData = $("<td>").text(response.Year);
-//   var actorsData = $("<td>").text(response.Actors);
-// // Append the td elements to the new table row
-// tRow.append(titleData, yearData, actorsData);
-// // Append the table row to the tbody element
-// $("tbody").append(tRow);
-// });
